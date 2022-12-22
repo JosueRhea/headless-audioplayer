ac2915d6d9767db55c5d520db69ff41c4efea821
Josue A <josueroyale8171@gmail.com>
2022-12-22 14:53:14 +0000

    Chore: update version

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,5 +1,5 @@
 import { AppProps } from "next/app";
-import 'headless-audioplayer-react/dist/cjs/css/slider.css'
+import "headless-audioplayer-react/dist/cjs/css/slider.css";
 import "../styles/globals.css";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
